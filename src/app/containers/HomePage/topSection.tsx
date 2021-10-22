2003f74197ac2e7ac434d3830a33fa35b8115cac
Karthik  Hebbar <96karthikhebbar@gmail.com>
2021-10-22 10:07:13 +0530

    added buttons to the top section and styled it

--- src/app/containers/HomePage/topSection.tsx
+++ src/app/containers/HomePage/topSection.tsx
@@ -5,6 +5,7 @@ import tw from "twin.macro";
 import MclarenCarImg from "../../../assets/images/mclaren-orange-big.png";
 import BlobImg from "../../../assets/images/blob.svg";
 import { SCREENS } from "../../components/responsive";
+import { Button } from "../../components/buttons";
 
 const TopSectionContainer = styled.div`
   min-height: 400px;
@@ -142,6 +143,12 @@ const StandaloneCar = styled.div`
   }
 `;
 
+const ButtonsContainer = styled.div`
+  ${tw`
+    flex
+  `};
+`;
+
 export function TopSection() {
   return (
     <TopSectionContainer>
@@ -152,6 +159,10 @@ export function TopSection() {
           at the best price for you and get the best quality cars for as long as
           you like.
         </Description>
+        <ButtonsContainer>
+          <Button text = "Book Your Ride" />
+          <Button theme= "filled" text = "Sell Your Car" />
+        </ButtonsContainer>
       </LeftContainer>
       <RightContainer>
         <BlobContainer>
